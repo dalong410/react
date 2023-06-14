4c0fba5330ed28b9970ffe38bb065dff9099b53a
saebom <dalong410@naver.com>
2023-06-14 15:55:05 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
+//import Hello from './Hello';
+//import Wrapper from './Wrapper';
+//import './App.css';
+
+function App() {
+  return (
+    <Counter />
+  );
+}
+
+export default App;
