755cf8c54cf7b7207fb5e4a5ea50cd2a44b5e356
saebom <dalong410@naver.com>
2023-06-15 13:52:40 +0900

    UserList 배열

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Counter from './Counter';
+import UserList from './UserList';
+//import InputSample from './InputSample';
+//import Counter from './Counter';
 //import Hello from './Hello';
 //import Wrapper from './Wrapper';
 //import './App.css';
 
 function App() {
   return (
-    <Counter />
+    <UserList />
   );
 }
 
